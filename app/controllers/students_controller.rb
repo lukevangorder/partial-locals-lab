6a06d99f6bc341ec94013201031b91161b6924e5
lukevangorder <lukevangorder@gmail.com>
2021-03-25 22:11:38 -0400

    commit tree 166ba9e5b0d1513d923cdad3c8620fb4fbe659d5

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,8 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    # @students = Student.where("name LIKE ?", "%params[:query]%")
+    @students = Student.search(params[:query])
   end
 
   def student_params
